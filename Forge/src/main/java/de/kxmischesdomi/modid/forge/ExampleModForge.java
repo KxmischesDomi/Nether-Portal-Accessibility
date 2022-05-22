0141e79655d849162e17c4d5cdf0b205e28b4f69
Dominik <67184131+KxmischesDomi@users.noreply.github.com>
2022-05-22 18:24:05 +0200

    Initialize 1.18

---
+++ Forge/src/main/java/de/kxmischesdomi/modid/forge/ExampleModForge.java
@@ -0,0 +1,19 @@
+package de.kxmischesdomi.modid.forge;
+
+import de.kxmischesdomi.modid.shared.ExampleModShared;
+import net.minecraftforge.fml.common.Mod;
+
+/**
+ * @author KxmischesDomi | https://github.com/kxmischesdomi
+ * @since 1.0
+ */
+@Mod("modid")
+public class ExampleModForge {
+
+	public ExampleModForge() {
+
+		ExampleModShared.init();
+
+	}
+
+}
